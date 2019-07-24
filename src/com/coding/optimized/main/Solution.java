4d95704b7ee70fa92743618c7af2955d399f2bc1
shamika050 <shamikadineshdha@gmail.com>
2019-07-24 23:43:02 +0800

    Assignment 01 - Driver Class

---
+++ src/com/coding/optimized/main/Solution.java
@@ -0,0 +1,13 @@
+package com.coding.optimized.main;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// Assignment 01
+		Bird bird = new BirdImpl();
+		bird.sing();	
+		
+
+	}
+
+}
